f0516771df98ab67450d98278622cacdd770784c
toadbalancer <github@toadbalancer.com>
2024-07-17 22:14:23 -0400

    Placeholder site, but this time in React

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div class="shadow-xl p-6 rounded-lg border-slate-700	border">
+      <h1 class="text-4xl font-bold text-slate-700">Toad</h1>
+      <h2 class="text-2xl font-normal text-slate-700">This is a placeholder—check back later!</h2>
+    </div>
+  );
+}
