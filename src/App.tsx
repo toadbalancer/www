b2ff528087d7dbdfae7a504a454decbbda1fa0f2
toadbalancer <github@toadbalancer.com>
2024-07-17 22:35:57 -0400

    Fix broken build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <div class="shadow-xl p-6 rounded-lg border-gray-100	border">
+    <div class="shadow-xl p-6 rounded-lg border-gray-100 border">
       <h1 class="text-4xl font-bold text-gray-800">Toad</h1>
       <h2 class="text-2xl font-normal text-gray-800">This is a placeholder—check back later!</h2>
     </div>
